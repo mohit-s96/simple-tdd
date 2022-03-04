c28f21cfe664e857674f72b2baff3fecea2d0ff0
msx47 <mohitsrivastava67@gmail.com>
2022-03-04 18:56:58 +0530

    feat: create add function, verify base case

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,17 @@
-import { greet } from "./index";
+import { Add, greet } from "./index";
 
 describe("checks if project setup is working", () => {
   test("greet function is compiled without errors", () => {
     expect(greet()).toBe("hello");
+  });
+});
+
+describe("verifies Add function is working accordingly", () => {
+  test("Add is a valid function exported properly", () => {
+    expect(typeof Add).toBe("function");
+  });
+
+  test("emty string returns zero", () => {
+    expect(Add("")).toBe(0);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function greet() {
   return "hello";
 }
+export function Add(input: string) {
+  let sum = 0;
+  return sum;
+}
